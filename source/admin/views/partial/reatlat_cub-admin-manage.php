be5bd63ab9cc764fb5fcb09f53b50fb51ea17c11
Alex Zappa <reatlat@gmail.com>
2017-12-10 23:18:57 +0100

    add remove ilnk, example link

--- source/admin/views/partial/reatlat_cub-admin-manage.php
+++ source/admin/views/partial/reatlat_cub-admin-manage.php
@@ -139,6 +139,7 @@
                 <td>Short Link</td>
                 <td>Full Link</td>
                 <td>Creator</td>
+                <td class="remove_link"></td>
             </tr>
             </thead>
             <tbody>
@@ -146,29 +147,48 @@
 
             $links = $plugin->get_links();
 
-            foreach ( $links as $key => $link )
+            if ( count($links) < 1 )
             {
-                $class = 'reatlat_cub_non-highlight';
-                if( !empty($_POST['submit_manage_links']) ) {
-                    $full_link = $plugin->get_full_link( $_POST['campaign_page'], $_POST['campaign_source'], $_POST['campaign_medium'], $_POST['campaign_name'], $_POST['custom_key_1'], $_POST['custom_value_1'], $_POST['custom_key_2'], $_POST['custom_value_2'], $_POST['custom_key_3'], $_POST['custom_value_3'] );
-                    if( !empty($_POST['submit_manage_links']) && !empty( $full_link ) && $full_link == $link->campaign_full_link ) $class = 'reatlat_cub_highlight';
-                }
+                // Demo example link
                 ?>
-                <tr class="<?php echo $class; ?> wow fadeInUp">
-                    <?php if ( strpos( esc_url_raw( $link->campaign_short_link ), 'https://goo.gl') !== false) { ?>
-                        <td data-info="true" class="campaign_info">
-                            <a target="_blank" title="Open Analytics data" href="<?php echo str_replace('https://goo.gl','https://goo.gl/info', esc_url_raw( $link->campaign_short_link ) ); ?>"><span class='dashicons dashicons-chart-area'></span></a>
-                        </td>
-                    <?php } else { ?>
-                        <td class="campaign_info"></td>
-                    <?php } ?>
-                    <td class="campaign_name"><strong><?php echo esc_attr( $link->campaign_name ); ?></strong></td>
-                    <td class="campaign_short_link" <?php if ( esc_url_raw( $link->campaign_short_link ) !== 'n/a' ) { ?>title="Copy to clipboard" data-clipboard-text="<?php echo esc_url_raw( $link->campaign_short_link ); ?>" data-copy="true" <?php } ?> ><?php echo esc_url_raw( $link->campaign_short_link ); ?><?php if ( esc_url_raw( $link->campaign_short_link ) !== 'n/a' ) { ?><span class="dashicons dashicons-clipboard"></span><?php } ?></td>
-                    <td class="campaign_full_link" title="Copy to clipboard" data-clipboard-text="<?php echo esc_url_raw( $link->campaign_full_link ); ?>" data-copy="true"><?php echo esc_url_raw( $link->campaign_full_link ); ?><span class="dashicons dashicons-clipboard"></span></td>
-                    <td class="user_id"><?php echo sanitize_user( get_userdata($link->user_id)->display_name ); ?> <small>(<?php echo esc_attr( implode(', ', get_userdata($link->user_id)->roles) ); ?>)</small></td>
+                <tr class="reatlat_cub_non-highlight wow fadeInUp">
+                    <td data-info="true" class="campaign_info">
+                        <a target="_blank" title="Open Analytics data" href="https://goo.gl/info/3br2tn"><span class='dashicons dashicons-chart-area'></span></a>
+                    </td>
+                    <td class="campaign_name"><strong>Example link</strong></td>
+                    <td class="campaign_short_link" title="Copy to clipboard" data-clipboard-text="https://goo.gl/3br2tn" data-copy="true" >https://goo.gl/3br2tn<span class="dashicons dashicons-clipboard"></span></td>
+                    <td class="campaign_full_link" title="Copy to clipboard" data-clipboard-text="https://wordpress.org/support/view/plugin-reviews/campaign-url-builder?rate=5#postform" data-copy="true">http://example.com/?utm_source=affiliate&utm_medium=banner&utm_campaign=Example+link<span class="dashicons dashicons-clipboard"></span></td>
+                    <td class="user_id">Alex Zappa <small>(Plugin Author)</small></td>
+                    <td class="remove_link demo"><span class="remove_link__inner"><span class="dashicons dashicons-no-alt"></span></span></td>
                 </tr>
                 <?php
+            } else {
+                foreach ( $links as $key => $link )
+                {
+                    $class = 'reatlat_cub_non-highlight';
+                    if( !empty($_POST['submit_manage_links']) ) {
+                        $full_link = $plugin->get_full_link( $_POST['campaign_page'], $_POST['campaign_source'], $_POST['campaign_medium'], $_POST['campaign_name'], $_POST['custom_key_1'], $_POST['custom_value_1'], $_POST['custom_key_2'], $_POST['custom_value_2'], $_POST['custom_key_3'], $_POST['custom_value_3'] );
+                        if( !empty($_POST['submit_manage_links']) && !empty( $full_link ) && $full_link == $link->campaign_full_link ) $class = 'reatlat_cub_highlight';
+                    }
+                    ?>
+                    <tr class="<?php echo $class; ?> wow fadeInUp" data-link-id="<?php echo esc_attr( $link->id ); ?>">
+                        <?php if ( strpos( esc_url_raw( $link->campaign_short_link ), 'https://goo.gl') !== false) { ?>
+                            <td data-info="true" class="campaign_info">
+                                <a target="_blank" title="Open Analytics data" href="<?php echo str_replace('https://goo.gl','https://goo.gl/info', esc_url_raw( $link->campaign_short_link ) ); ?>"><span class='dashicons dashicons-chart-area'></span></a>
+                            </td>
+                        <?php } else { ?>
+                            <td class="campaign_info"></td>
+                        <?php } ?>
+                        <td class="campaign_name"><strong><?php echo esc_attr( $link->campaign_name ); ?></strong></td>
+                        <td class="campaign_short_link" <?php if ( esc_url_raw( $link->campaign_short_link ) !== 'n/a' ) { ?>title="Copy to clipboard" data-clipboard-text="<?php echo esc_url_raw( $link->campaign_short_link ); ?>" data-copy="true" <?php } ?> ><?php echo esc_url_raw( $link->campaign_short_link ); ?><?php if ( esc_url_raw( $link->campaign_short_link ) !== 'n/a' ) { ?><span class="dashicons dashicons-clipboard"></span><?php } ?></td>
+                        <td class="campaign_full_link" title="Copy to clipboard" data-clipboard-text="<?php echo esc_url_raw( $link->campaign_full_link ); ?>" data-copy="true"><?php echo esc_url_raw( $link->campaign_full_link ); ?><span class="dashicons dashicons-clipboard"></span></td>
+                        <td class="user_id"><?php echo sanitize_user( get_userdata($link->user_id)->display_name ); ?> <small>(<?php echo esc_attr( implode(', ', get_userdata($link->user_id)->roles) ); ?>)</small></td>
+                        <td class="remove_link"><span class="dashicons dashicons-no-alt"></span></td>
+                    </tr>
+                    <?php
+                }
             }
+
             ?>
             </tbody>
         </table>
