19640b95307b33e6ee1d9d2828b12cff4501e8e6
Alex Zappa <reatlat@gmail.com>
2018-09-10 01:51:12 +0200

    Fixed bug with plugin removal function

--- source/uninstall.php
+++ source/uninstall.php
@@ -16,13 +16,13 @@ if(  is_admin() ) {
         $wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . $plugin_name . '_mediums' );
         $wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . $plugin_name . '_sources' );
 
-        delete_option( $this->plugin_name . '_version' );
-        delete_option( $this->plugin_name . '_advanced_api' );
-        delete_option( $this->plugin_name . '_bitly_api_key' );
-        delete_option( $this->plugin_name . '_google_api_key' );
-        delete_option( $this->plugin_name . '_admin_only' );
-        // delete_option( $this->plugin_name . '_keep_settings' );
-        delete_option( $this->plugin_name . '_show_creator' );
-        delete_option( $this->plugin_name . '_show_useronly' );
+        delete_option( $plugin_name . '_version' );
+        delete_option( $plugin_name . '_advanced_api' );
+        delete_option( $plugin_name . '_bitly_api_key' );
+        delete_option( $plugin_name . '_google_api_key' );
+        delete_option( $plugin_name . '_admin_only' );
+        // delete_option( $plugin_name . '_keep_settings' );
+        delete_option( $plugin_name . '_show_creator' );
+        delete_option( $plugin_name . '_show_useronly' );
     }
 }
