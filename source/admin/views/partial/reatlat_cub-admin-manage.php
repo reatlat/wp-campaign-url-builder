7faad2c6aa1965e8fe5199300cb4972d09eb4518
Alex Zappa <reatlat@gmail.com>
2018-10-20 20:39:41 +0200

    move button only to mange tab

--- source/admin/views/partial/reatlat_cub-admin-manage.php
+++ source/admin/views/partial/reatlat_cub-admin-manage.php
@@ -2,6 +2,8 @@
 
     <h3><?php _e('Existing generated links', 'campaign-url-builder'); ?></h3>
 
+    <button name="export_to_csv" class="export_to_csv js-export_to_csv"><span class="dashicons dashicons-media-spreadsheet"></span> <?php _e('Export CSV', 'campaign-url-builder'); ?></button>
+
     <?php include dirname( __FILE__ ) . '/reatlat_cub-admin-links-list.php'; ?>
 
 </div>
