dbdaff1b8562c7f5fd865b694a1b38e41e50debe
Alex Zappa <reatlat@gmail.com>
2018-10-21 04:45:36 +0200

    add pagination buttons

--- source/admin/views/partial/reatlat_cub-admin-manage.php
+++ source/admin/views/partial/reatlat_cub-admin-manage.php
@@ -2,6 +2,12 @@
 
     <h3><?php _e('Existing generated links', 'campaign-url-builder'); ?></h3>
 
+    <div class="reatlat_cub_result__table_pagination" data-current-page="1">
+        <span class="table_pagination__currentpage"></span>&nbsp;
+        <button name="sortTable--prev" class="js-sortTable--prev"><span class="dashicons dashicons-arrow-left-alt2"></span></button>
+        <button name="sortTable--next" class="js-sortTable--next"><span class="dashicons dashicons-arrow-right-alt2"></span></button>
+    </div>
+
     <button name="export_to_csv" class="export_to_csv js-export_to_csv"><span class="dashicons dashicons-media-spreadsheet"></span> <?php _e('Export CSV', 'campaign-url-builder'); ?></button>
 
     <?php include dirname( __FILE__ ) . '/reatlat_cub-admin-links-list.php'; ?>
