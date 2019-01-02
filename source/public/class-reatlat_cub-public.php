d60d190c558fcadf02d9b57319ebfac2de553117
Alex Zappa <reatlat@gmail.com>
2019-01-02 21:11:50 +0100

    WIP

--- source/public/class-reatlat_cub-public.php
+++ source/public/class-reatlat_cub-public.php
@@ -33,39 +33,43 @@ class reatlat_cub_Public
      */
 	public function register_shortcodes()
     {
-        function shortcode_frontend_link_generator_form( $atts ) {
+        function shortcode_frontend_link_generator_form( $atts )
+        {
             $atts = shortcode_atts( array(
                 'wrapper' => '', // set specific CSS classes for add specific stile for your form
+                'wrapper-inline-style' => 'false',
                 'form' => '', // set specific CSS classes for add specific stile for your form
+                'form-inline-style' => 'false',
                 'type' => 'default', // default | simple | advanced | preset
                 'utm_source' => '',
                 'utm_medium' => '',
                 'utm_campaign' => '',
                 'utm_term' => '',
                 'utm_content' => '',
-                '' => '',
-                '' => '',
-                '' => '',
-                'recaptcha' => 'false',
-                'inline-style' => 'false'
+                'custom_parameters' => '', // any custom parameters separate by | Vertical bar https://en.wikipedia.org/wiki/Vertical_bar
+                'recaptcha' => 'false', // TODO: add recaptcha v3
             ), $atts, 'Campaign-URL-Builder' );
 
             ob_start();
+
             ?>
 
-            <div class="Campaign-URL-Builder Campaign-URL-Builder--form-wrapper <?php echo $atts['wrapper']; ?>" <?php if ( $atts['inline-style'] !== 'false' ) { echo 'style="' . $atts['inline-style'] . '"'; } ?>>
-                <form action="#" method="post" class="Campaign-URL-Builder--form-wrapper__form <?php echo $atts['form']; ?>">
+            <div class="Campaign-URL-Builder Campaign-URL-Builder--form-wrapper <?php echo $atts['wrapper']; ?>" <?php if ( $atts['wrapper-inline-style'] !== 'false' ) { echo 'style="' . $atts['wrapper-inline-style'] . '"'; } ?>>
+                <form action="#" method="post" class="Campaign-URL-Builder--form-wrapper__form <?php echo $atts['form']; ?>" <?php if ( $atts['form-inline-style'] !== 'false' ) { echo 'style="' . $atts['form-inline-style'] . '"'; } ?>>
+
                     <input type="text">
                     <input type="submit" value="send">
+
                 </form>
             </div>
 
             <?php
+
             $content = ob_get_contents();
+
             ob_end_clean();
 
             return $content;
-
         }
         add_shortcode( 'Campaign-URL-Builder', 'shortcode_frontend_link_generator_form' );
     }
